a57b76cc4fb799a4e2e8d69dfb68a9ae7c80646f
hetao <847957840qq.com>
2020-10-16 17:11:34 +0800

    切换路径

---
+++ spring-framework/spring-annopriciple/src/main/java/com/misty/spring/compentscan/dao/MistyDao.java
@@ -0,0 +1,14 @@
+package com.misty.spring.compentscan.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @ClassName MistyDao
+ * @Description TODO
+ * @Author HeTao
+ * @Date 2020/10/16 15:54
+ * @Version 1.0
+ **/
+@Repository
+public class MistyDao {
+}
---
+++ spring-framework/spring-annopriciple/src/main/java/com/misty/spring/compentscan/service/impl/MistyServiceImpl.java
@@ -0,0 +1,15 @@
+package com.misty.spring.compentscan.service.impl;
+
+import com.misty.spring.compentscan.service.MistyService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName MistyService
+ * @Description TODO
+ * @Author HeTao
+ * @Date 2020/10/16 15:55
+ * @Version 1.0
+ **/
+@Service
+public class MistyServiceImpl implements MistyService {
+}
